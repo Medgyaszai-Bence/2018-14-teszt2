2c6b6babd884e00af3514a009d600efd809a9f1f
Petrik Lajos Szg <info@petrik.hu>
2018-09-21 16:20:52 +0200

    4.feladat

--- GitConsoleApp/Program.cs
+++ GitConsoleApp/Program.cs
@@ -10,38 +10,45 @@ namespace GitConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Adja meg hány számot szeretne bekérni.");
-            
-            int bekeres = Convert.ToInt32(Console.ReadLine());
-            int[] t = new int[bekeres];
+            try {
+                Console.WriteLine("Adja meg hány számot szeretne bekérni.");
 
-            Console.WriteLine("Kérem adjon meg {0} számot.", bekeres);
+                int bekeres = Convert.ToInt32(Console.ReadLine());
+                int[] t = new int[bekeres];
 
-            int szamok;
-            for (int i = 0; i < t.Length; i++)
-            {
-                Console.WriteLine("Kérem adja meg az {0} számot", i+1);
-                szamok = Convert.ToInt32(Console.ReadLine());
-                t[i] = szamok;
-                
+                Console.WriteLine("Kérem adjon meg {0} számot.", bekeres);
+
+                int szamok;
+                for (int i = 0; i < t.Length; i++)
+                {
+                    Console.WriteLine("Kérem adja meg az {0} számot", i + 1);
+                    szamok = Convert.ToInt32(Console.ReadLine());
+                    t[i] = szamok;
+
+                }
+                for (int i = 0; i < t.Length; i++)
+                {
+                    Console.WriteLine(t[i]);
+                }
+                int max, min;
+
+                max = t[0];
+                min = t[0];
+                for (int i = 1; i < t.Length; i++)
+
+                {
+
+                    if (t[i] > max) max = t[i];
+                    if (t[i] < min) min = t[i];
+
+                }
+                Console.WriteLine("Legnagyobb szám: {0} \nLegkissebb szám: {1}", max, min);
             }
-            for (int i = 0; i < t.Length; i++)
+            catch (Exception )
             {
-                Console.WriteLine(t[i]);
+                Console.WriteLine("Ez nem értelmezhető!");
+                return;
             }
-            int max, min;
-
-            max = t[0];
-            min = t[0];
-            for (int i = 1; i < t.Length; i++)
-
-            {
-
-                if (t[i] > max) max = t[i];
-                if (t[i] < min) min = t[i];
-
-            }
-            Console.WriteLine("Legnagyobb szám: {0} \nLegkissebb szám: {1}", max,min);
 
         }
     }
